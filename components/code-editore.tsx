4a94499108ed40dbd74912a8669039ab80dc0aaf
0uali-Yassine <yassinzerouali0@gmail.com>
2025-05-21 11:00:40 +0100

    add dependencies for pyodide & skulpt

--- components/code-editore.tsx
+++ components/code-editore.tsx
@@ -6,7 +6,7 @@ import { css } from '@codemirror/lang-css';
 import { javascript } from '@codemirror/lang-javascript';
 import { dracula } from '@uiw/codemirror-theme-dracula';
 import { githubLight } from '@uiw/codemirror-theme-github';
-import { ChevronUp, ChevronDown, Play, RotateCcw, Sun, Moon, Maximize2, Minimize2 } from 'lucide-react';
+import {  Play, RotateCcw, Sun, Moon, Maximize2, Minimize2 } from 'lucide-react';
 
 const CodeEditore = () => {
   const [code, setCode] = useState(`# Start coding here
@@ -21,6 +21,7 @@ print("Hello World!")
   const [isDragging, setIsDragging] = useState(false);
   const editorContainerRef = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
+  
 
   const languages = [
     { id: 'python', name: 'Python', icon: '🐍' },
